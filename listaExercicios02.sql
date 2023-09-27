e16ddcc4c029a0c627b59fab777d0cb13f2a8c5f
Marirsil <110177157+Marirsil@users.noreply.github.com>
2023-09-26 22:15:12 -0300

    Update listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -181,3 +181,13 @@ BEGIN
     FROM Autor;
 END //
 DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
