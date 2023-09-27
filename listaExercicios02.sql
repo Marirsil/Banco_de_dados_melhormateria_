31742aab3110325399832fd92fbd0cd47870c81d
Marirsil <110177157+Marirsil@users.noreply.github.com>
2023-09-26 22:29:15 -0300

    10. Livros e Seus Autores

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -280,3 +280,13 @@ BEGIN
     WHERE Categoria.Nome = categoriaNome;
 END //
 DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS NomeAutor
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+DELIMITER ;
